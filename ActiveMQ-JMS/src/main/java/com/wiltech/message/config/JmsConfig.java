693d8facabf1d9fd90efe1f57a6f6b2aaebd47bb
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-12-06 14:31:39 +0000

    Added message converter

---
+++ ActiveMQ-JMS/src/main/java/com/wiltech/message/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.wiltech.message.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Configure JMS message properties. Here the message type can be set. This is used to convet from Java POJOs to Messages.
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
