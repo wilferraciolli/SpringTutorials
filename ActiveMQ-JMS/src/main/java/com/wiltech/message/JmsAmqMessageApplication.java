8179aca0bec75b58f8d56cb658dad3f28dfb2796
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-12-06 16:16:13 +0000

    Removed embedded active mq server Connected active mq server to application

--- ActiveMQ-JMS/src/main/java/com/wiltech/message/JmsAmqMessageApplication.java
+++ ActiveMQ-JMS/src/main/java/com/wiltech/message/JmsAmqMessageApplication.java
@@ -1,8 +1,5 @@
 package com.wiltech.message;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +8,14 @@ public class JmsAmqMessageApplication {
 
     public static void main(String[] args) throws Exception  {
 
-        // start the embedded server to test. PS do not do this in prod
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-
-        server.start();
+//        // start the embedded server to test. PS do not do this in prod
+        //        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+        //                .setPersistenceEnabled(false)
+        //                .setJournalDirectory("target/data/journal")
+        //                .setSecurityEnabled(false)
+        //                .addAcceptorConfiguration("invm", "vm://0"));
+        //
+        //        server.start();
 
         SpringApplication.run(JmsAmqMessageApplication.class, args);
     }
