543355cf963ec9994973d592857cb7238ce55462
Wiliam Ferraciolli <wiliam334@hotmail.com>
2020-02-18 05:19:25 +0000

    Spring Security Microservices - Initial commit added parent project

---
+++ SpringSecurityMicroservices/Read me.md
@@ -0,0 +1,10 @@
+# Getting Started
+
+### Reference Documentation
+Tutorial showing how to set up
+Gateway Service
+Service discovery
+Spring Security for Microservices
+
+Maven modular applications
+
---
+++ SpringSecurityMicroservices/src/test/java/com/wiltech/microservices/security/SpringSecurityMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.microservices.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
