e1c9838b2ab95d0c3899f2516f415d67eac84f9a
wilferraciolli <wiliam334@hotmail.com>
2023-03-30 23:41:38 +0300

    Spring native - initial commit

---
+++ spring-native/src/test/java/com/wiltech/springnative/SpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.springnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
