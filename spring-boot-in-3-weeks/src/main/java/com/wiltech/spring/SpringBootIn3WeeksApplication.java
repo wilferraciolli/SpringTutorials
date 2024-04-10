f55ac63c5b2eb7dc5d80848fd06634a9343ed16c
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-04-10 16:38:34 +0100

    Spring boot in 3 weeks - Initial commit

---
+++ spring-boot-in-3-weeks/src/main/java/com/wiltech/spring/SpringBootIn3WeeksApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn3WeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn3WeeksApplication.class, args);
+	}
+
+}
