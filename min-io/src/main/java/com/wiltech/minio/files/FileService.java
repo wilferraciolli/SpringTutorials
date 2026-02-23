07c8192bd6e1887f43d155dbd0372e86768e0f8b
WilFerraciolli <wiliam334@hotmail.com>
2026-02-23 23:19:26 +0200

    min-io - Added metadata to files

--- min-io/src/main/java/com/wiltech/minio/files/FileService.java
+++ min-io/src/main/java/com/wiltech/minio/files/FileService.java
@@ -12,12 +12,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class FileService {
+    public static final String ORIGINAL_FILENAME = "original-filename";
     @Autowired
     private MinioClient minioClient;
 
@@ -25,7 +25,7 @@ public class FileService {
     private String bucketName;
 
     public String uploadFile(final MultipartFile file) throws Exception {
-        // create bucket if not exiss
+        // create bucket if not exists
         boolean found = minioClient.bucketExists(BucketExistsArgs.builder()
                 .bucket(bucketName)
                 .build());
@@ -36,19 +36,30 @@ public class FileService {
                     .build());
         }
 
-        String fileName = System.currentTimeMillis()
-                + "_"
-                + file.getOriginalFilename();
+        String originalFileName = file.getOriginalFilename();
+        String extension = "";
+        if (originalFileName != null && originalFileName.contains(".")){
+            extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        }
+
+        String uuidName = UUID.randomUUID() + extension;
+
+        // Create file metadata
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put(ORIGINAL_FILENAME, originalFileName);
+        metadata.put("extension", extension);
 
         minioClient.putObject(
                 PutObjectArgs.builder()
                         .bucket(bucketName)
-                        .object(fileName)
+                        .object(uuidName)
                         .stream(file.getInputStream(), file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .userMetadata(metadata)
                         .build()
         );
 
-        return "File uploaded successfully: " + fileName;
+        return "File uploaded successfully: " + uuidName;
     }
 
     public List<String> listAllFiles() throws Exception {
@@ -67,13 +78,20 @@ public class FileService {
         return filenames;
     }
 
-    public String getDownloadLink(final String fileName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+    public String getDownloadLink(final String uuidName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        String originalName = getOriginalFileName(uuidName);
+
+        Map<String, String> reqParams = new HashMap<>();
+        // This tells the browser: "Save this as 'original name.jpg' even though the URL is a UUID"
+        reqParams.put("response-content-disposition", "attachment; filename=\"" + originalName + "\"");
+
         return minioClient.getPresignedObjectUrl(
                 GetPresignedObjectUrlArgs.builder()
                         .method(Method.GET)
                         .bucket(bucketName)
-                        .object(fileName)
+                        .object(uuidName)
                         .expiry(2, TimeUnit.HOURS) // to expiry in 2 hours
+                        .extraQueryParams(reqParams)
                         .build()
         );
     }
@@ -102,5 +120,17 @@ public class FileService {
         } catch (Exception e) {
             return false; // File doesn't exist or other error
         }
+    }
+
+    public String getOriginalFileName(final String uuidName) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        StatObjectResponse stat = minioClient.statObject(
+                StatObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(uuidName)
+                        .build()
+        );
+
+        // MinIO prefixes user metadata with "X-Amz-Meta-", so make sure to manage it
+    return stat.userMetadata().get(ORIGINAL_FILENAME);
     }
 }
