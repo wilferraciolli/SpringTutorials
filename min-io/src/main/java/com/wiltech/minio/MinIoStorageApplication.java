f179e4077a01197907d58a307a2368011cec1d8b
WilFerraciolli <wiliam334@hotmail.com>
2026-02-23 22:06:57 +0200

    min-io - initial set up

---
+++ min-io/src/main/java/com/wiltech/minio/MinIoStorageApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinIoStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinIoStorageApplication.class, args);
+	}
+
+}
---
+++ min-io/src/test/java/com/wiltech/minio/MinIoStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiltech.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
